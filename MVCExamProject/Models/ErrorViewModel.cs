dab5accfb87eebf4382b9b25f7bb08ebf17429c5
Aya Adel <ayaadelt3@gmail.com>
2023-06-25 16:11:35 +0300

    Add project files.

---
+++ MVCExamProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCExamProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

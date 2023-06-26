a673989a1fb077cfdc24e77813a35cbe81d57090
Kareem Hesham <karemmanso@gmail.com>
2023-06-26 17:23:52 +0300

    adding course controller and view(beta)

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCExamProject.Models;
+
+namespace MVCExamProject.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        [HttpPost]
+        public IActionResult Index(ContactUs contact)
+        {
+            if (ModelState.IsValid)
+            {
+                // Process the contact form submission (e.g., send an email)
+
+                // Optionally, redirect to a "Thank You" page
+                return RedirectToAction("ThankYou");
+            }
+
+            return View(contact);
+        }
+
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+
+}

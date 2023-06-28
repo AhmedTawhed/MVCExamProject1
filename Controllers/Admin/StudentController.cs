70b00f4359f657b57ca4d09a289343f53ad48752
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2023-06-29 00:23:13 +0300

    edite student

--- Controllers/Admin/StudentController.cs
+++ Controllers/Admin/StudentController.cs
@@ -2,7 +2,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCExamProject.Models;
 using MVCExamProject.Repository.Interfaces;
-=======
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;

5df58b7dace9b7fc06c601295890e3b9c7a9447b
hasnaaamer <105937874+hasnaaamer@users.noreply.github.com>
2023-06-29 16:34:47 +0200

    Sign in / out view and authentication

--- ViewModel/SignUPUserViewModel.cs
+++ ViewModel/SignUPUserViewModel.cs
@@ -1,15 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MVCExamProject.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCExamProject.ViewModel
 {
-    public class SignUPUserViewModel
-    {
-        public string Name { get; set; }
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-        public string? Email { get; set; }
-        [DataType(DataType.Password)]
-        [Compare("Password")]
-        public string ConfirmPassword { get; set; }
-    }
+	public class SignUPUserViewModel
+	{
+		[MaxLength(25)]
+		[MinLength(3)]
+		[Required]
+		public string Name { get; set; }
+		public int? Age{get; set;}
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		[MaxLength(40)]
+		
+
+		[DataType(DataType.Password)]
+		[Compare("Password")]
+		public string ConfirmPassword { get; set; }
+
+		public bool IsAdmin { get; set; } = false;
+		public List<UserExam>? UserExams { get; set; }
+	}
+
 }

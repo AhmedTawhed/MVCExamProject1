3008924709c854a9c8238861800eae1d6bdf6d21
Kareem Hesham <karemmanso@gmail.com>
2023-06-29 17:00:07 +0300

    updated mark's student controller

--- Controllers/Admin/StudentController.cs
+++ Controllers/Admin/StudentController.cs
@@ -1,23 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MVCExamProject.Models;
+using MVCExamProject.Repository.Interfaces;
+
 
 namespace MVCExamProject.Controllers.Admin
 {
-    public class StudentController : Controller
-    {
-        //[Authorize("Admin")]
-        [Route("admin/students")]
-        public IActionResult Index()
-        {
+	[Authorize(Roles = "Admin")]
+	public class StudentController : Controller
+	{
+		private readonly IUserRepository _userRepository;
 
-            return View("~/Views/Admin/Student/index.cshtml");
-        }
+		public StudentController(IUserRepository userRepository)
+		{
+			_userRepository = userRepository;
+		}
 
-        //[Authorize("Admin")]
-        [Route("admin/students/show")]
+
+
+
+		//[Authorize("Admin")]
+		[Route("admin/students")]
+		public IActionResult Index()
+		{
+			List<User> students = _userRepository.GetAll();
+			return View("~/Views/Admin/Student/index.cshtml", students);
+			return View("~/Views/Admin/Students/Index.cshtml", students);
+		}
+
+		//[Authorize("Admin")]
+		[Route("admin/students/show")]
 		public IActionResult Show(int id)
 		{
 
 			return View("~/Views/Admin/Student/Show.cshtml");
 		}
+		//getbyid
+		public IActionResult viewDetails(int id)
+		{
+			User student = _userRepository.GetById(id);
+			return View(student);
+		}
+
+		//[Authorize("Admin")]
+		[Route("admin/students/delete")]
+		public IActionResult Delete(int id)
+		{
+			User student = _userRepository.GetById(id);
+
+			if (student != null)
+			{
+
+
+				_userRepository.Delete(student);
+				//save????????????????
+
+				List<User> students = _userRepository.GetAll();
+				return View("~/Views/Admin/students/index.cshtml", students);
+
+			}
+			return View("~/Views/Admin/students/index.cshtml");
+		}
+
 	}
 }

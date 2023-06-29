f18eba49e8494868257b9dcf9cc5adc3be190df2
Kareem Hesham <karemmanso@gmail.com>
2023-06-29 18:16:06 +0300

    from new branch

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using MVCExamProject.Data;
 using MVCExamProject.Repository;
@@ -5,42 +6,53 @@ using MVCExamProject.Repository.Interfaces;
 
 namespace MVCExamProject
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddDbContext<ExamContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("Exams")));
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+			builder.Services.AddDbContext<ExamContext>(options =>
+			options.UseSqlServer(builder.Configuration.GetConnectionString("Exams")));
 
-            builder.Services.AddScoped<IAdminRepository, AdminService>();
-            builder.Services.AddScoped<IUserRepository, UserService>();
-            builder.Services.AddScoped<IContactUsRepository, ContactUsService>();
+			//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+			//   .AddCookie();
 
-            var app = builder.Build();
+			builder.Services.AddScoped<IAdminRepository, AdminService>();
+			builder.Services.AddScoped<IUserRepository, UserService>();
+			builder.Services.AddScoped<IContactUsRepository, ContactUsService>();
+			builder.Services.AddScoped<IExamRepository, ExamService>();
+			builder.Services.AddScoped<IExamQuestionRepository, ExamQuestionService>();
+			builder.Services.AddScoped<IQuestionOptionRepository, QuestionOptionService>();
+			builder.Services.AddScoped<IStudentRepository, StudentService>();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+			builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
-            app.UseRouting();
 
-            app.UseAuthorization();
+			var app = builder.Build();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
 
-            app.Run();
-        }
-    }
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
 }

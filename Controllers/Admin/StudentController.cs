3fcbb185c701dea76a4845881f590ca1cd16c21b
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2023-06-30 03:53:12 +0300

    edit student view

--- Controllers/Admin/StudentController.cs
+++ Controllers/Admin/StudentController.cs
@@ -25,7 +25,7 @@ namespace MVCExamProject.Controllers.Admin
         public IActionResult Index()
         {
             List<User> students = _userRepository.GetAll();
-            return View("~/Views/Admin/students/index.cshtml", students);
+            return View("~/Views/Admin/Student/index.cshtml", students);
         }
 
         //getbyid
@@ -45,10 +45,10 @@ namespace MVCExamProject.Controllers.Admin
             {
 
                 _userRepository.Delete(student);
-
+                return RedirectToAction("Index");
 
             }
-            return View("~/Views/Admin/students/index.cshtml");
+            return View("~/Views/Admin/Student/index.cshtml");
         }
 
     }
